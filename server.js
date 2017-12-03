6f6ddaa662f608403bcf3f12bf4cab1dd250b649
kevindhir <kevindhir@alumni.ubc.ca>
2017-12-02 19:04:25 -0800

    another approach

--- server.js
+++ server.js
@@ -8,22 +8,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema,
     ObjectID = Schema.ObjectID;
 
-const allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-        res.send(200);
-    }
-    else {
-        next();
-    }
-};
-
-
-app.use(allowCrossDomain);
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 
 const mongoUri = 'mongodb://localhackday:testpassword@ds044667.mlab.com:44667/ubclocalhackday';
